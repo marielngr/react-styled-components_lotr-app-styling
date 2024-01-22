d94c1efaca594638afa0909de3d6b21def33ebd8
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-01-22 22:59:02 +0100

    add several styles

--- pages/volumes/[slug].js
+++ pages/volumes/[slug].js
@@ -2,6 +2,47 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { volumes } from "../../lib/data";
+import styled from "styled-components";
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding: 2rem;
+`;
+
+const Section = styled.section`
+  display: flex;
+  justify-content: space-around;
+  padding: 2rem;
+  color: white;
+  background-color: ${({ $bgcolor }) => $bgcolor};
+  margin-left: -3rem;
+  margin-right: -3rem;
+`;
+
+const StyledUl = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  line-height: 150%;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const StyledNav = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const StyledImage = styled(Image)`
+  border: 2px solid green;
+  margin-top: 2.5rem;
+`;
 
 export default function VolumeDetail() {
   const router = useRouter();
@@ -17,50 +58,65 @@ export default function VolumeDetail() {
     return null;
   }
 
-  const { title, description, cover, books } = volume;
+  const { title, description, cover, books, color } = volume;
+  console.log(title, color);
 
   return (
-    <>
-      <Link href="/volumes">
-        <Image src="/images/chevron-left.svg" alt="" width={20} height={20} />
+    <FlexContainer>
+      <StyledLink href="/volumes">
+        <StyledImage
+          src="/images/chevron-left.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
         All Volumes
-      </Link>
+      </StyledLink>
       <h1>{title}</h1>
       <p>{description}</p>
-      <ul>
-        {books.map(({ ordinal, title }) => (
-          <li key={title}>
-            {ordinal}: <strong>{title}</strong>
-          </li>
-        ))}
-      </ul>
-      <Image
-        src={cover}
-        alt={`Cover image of ${title}`}
-        width={140}
-        height={230}
-      />
+      <Section $bgcolor={color}>
+        <StyledUl>
+          {books.map(({ ordinal, title }) => (
+            <li key={title}>
+              {ordinal} <br />
+              <strong>{title}</strong>
+            </li>
+          ))}
+        </StyledUl>
+        <Image
+          src={cover}
+          alt={`Cover image of ${title}`}
+          width={140}
+          height={230}
+        />
+      </Section>
+
       {previousVolume ? (
         <div>
-          <Link href={`/volumes/${previousVolume.slug}`}>
-            <Image src="/images/arrow-left.svg" alt="" width={20} height={20} />
+          <StyledLink href={`/volumes/${previousVolume.slug}`}>
+            <StyledImage
+              src="/images/arrow-left.svg"
+              alt=""
+              width={20}
+              height={20}
+            />
             Previous Volume: {previousVolume.title}
-          </Link>
+          </StyledLink>
         </div>
       ) : null}
       {nextVolume ? (
-        <div>
-          <Link href={`/volumes/${nextVolume.slug}`}>
+        <StyledNav>
+          <StyledLink href={`/volumes/${nextVolume.slug}`}>
             Next Volume: {nextVolume.title}
-            <Image
+            <StyledImage
               src="/images/arrow-right.svg"
               alt=""
               width={20}
               height={20}
             />
-          </Link>
-        </div>
+          </StyledLink>
+        </StyledNav>
       ) : null}
-    </>
+    </FlexContainer>
   );
 }
